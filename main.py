342a7bdf3c2fa219ce539b6dfaf5cd4b23138691
bylat <bylat.anyrovich@gmail.com>
2022-12-18 10:39:36 +0500

    3 коммит

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 
 
 mass = ['wqeeq', '22', '232', 'ewewe', '1']
-mass2 = []
 
-for i in mass:
-    if len(i) <= 3:
-        print('1')
+def fun_m(mass):
+    mass2 = []
+    for i in mass:
+        if len(i) <= 3:
+            mass2.append(i)
+    return mass2
+
+print(fun_m(mass))
